fe7f89d26539605986b01d288aee8c6243986822
kamila <kamilakmiecik01@gmail.com>
2025-07-18 20:44:11 +0200

    fixed seeding, added get controllers for tests

---
+++ PetFinder/Controllers/PetAppUsersController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PetFinder.Data;
+using PetFinder.Entities;
+
+namespace PetFinder.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PetAppUsersController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public PetAppUsersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IReadOnlyList<PetAppUser>> Index()
+        {
+            return Ok(_context.PetUsers.ToList());
+        }
+        
+        
+        [HttpGet("{id}")]
+        public ActionResult<PetAppUser> GetByID(string id)
+        {
+            var pet = _context.PetUsers.Find(id);
+
+            if(pet==null)
+                return NotFound();
+
+            return Ok(pet);
+        }
+
+    
+    }
+}
--- PetFinder/Program.cs
+++ PetFinder/Program.cs
@@ -6,9 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
@@ -16,14 +13,21 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 }); 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+using (var scope = app.Services.CreateScope())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<AppDbContext>();
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while seeding the database.");
+    }
 }
-
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
