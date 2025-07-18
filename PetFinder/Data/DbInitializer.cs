0bbb78d2fbcaff9fedb6c80d8259735b64232cf7
kamila <kamilakmiecik01@gmail.com>
2025-07-18 20:24:13 +0200

    added seeding

---
+++ PetFinder/Data/DbInitializer.cs
@@ -0,0 +1,47 @@
+﻿using PetFinder.Entities;
+
+namespace PetFinder.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(AppDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.PetUsers.Any())
+            {
+                return;
+            }
+
+            var users = new PetAppUser[]
+            {
+                new PetAppUser
+                {
+                    ID = Guid.NewGuid().ToString(),
+                    DisplayName = "Azor",
+                    ContactEmail = "123@gmail.com",
+                    IsPet = true,
+                },
+                new PetAppUser
+                {
+                    ID = Guid.NewGuid().ToString(),
+                    DisplayName = "Moris",
+                    ContactEmail = "123@gmail.com",
+                    IsPet = true,
+                },
+                new PetAppUser
+                {
+                    ID = Guid.NewGuid().ToString(),
+                    DisplayName = "Anna",
+                    ContactEmail = "123@gmail.com",
+                    IsPet = false,
+                },
+
+
+            };
+
+            context.PetUsers.AddRange(users);
+            context.SaveChanges();
+        }
+    }
+}
